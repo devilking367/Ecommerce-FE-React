9c8959dc1b7aa0fa60bdb7baeba484f470167782
Dung Dam <s>
2025-07-18 20:52:26 +0700

    feature: update side bar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,10 +2,9 @@ import clsx from "clsx";
 import { useState } from "react";
 // icons
 import { GoHome } from "react-icons/go";
-import { RiArrowLeftDoubleFill } from "react-icons/ri";
+import { RiArrowLeftDoubleFill, RiArrowRightDoubleFill } from "react-icons/ri";
 import { AiFillProduct } from "react-icons/ai";
 import { MdOutlineCategory } from "react-icons/md";
-
 
 import { LuUsers } from "react-icons/lu";
 import { useLocation } from "react-router-dom";
@@ -41,12 +40,14 @@ const sidebarItems: SideNavItemType[] = [
 
 export default function Sidebar() {
   const [isSidebarOpen, setSidebarOpen] = useState(true);
-
+console.log('isSidebarOpen: ', isSidebarOpen)
   return (
+    <div className="relative border-r-[1px] md:pr-8 pr-3 pt-2">
+
     <div
       className={clsx(
-        "relative min-h-screen max-h-screen overflow-y-auto md:pr-8 pr-3 pt-2 flex flex-col gap-3 border-r-[1px] px-[10px]",
-        isSidebarOpen ? "md:w-[300px]" : "md:w-[80px]"
+        " min-h-screen max-h-screen overflow-y-auto  flex flex-col gap-3  px-[10px]",
+        isSidebarOpen ? "md:w-[300px]" : "md:w-[50px]"
       )}
     >
       {/* Logo */}
@@ -86,12 +87,13 @@ export default function Sidebar() {
       <button
         onClick={() => setSidebarOpen(!isSidebarOpen)}
         className={clsx(
-          "absolute top-1/9 -translate-y-1/2 z-9999 right-[-16px] z-50 bg-white border border-gray-300 rounded-full p-2 shadow-md transition-all z-50",
-          isSidebarOpen ? "text-2xl" : "text-xl rotate-180"
+          "absolute top-1/7 -translate-y-1/2 z-9999 right-[-16px] cursor-pointer bg-white border border-black rounded-full p-2 shadow-md transition-all",
+          isSidebarOpen ? "text-2xl" : "text-l rotate-180"
         )}
       >
         <RiArrowLeftDoubleFill className="text-gray-500" />
       </button>
+    </div>
     </div>
   );
 }
